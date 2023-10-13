d095f7d38f53e2093aadd2644e1a3c4a0faa1bb2
ken1117a <147683828+ken1117a@users.noreply.github.com>
2023-10-13 12:58:55 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,14 @@
+public abstrate class TangibleAsset{
+  private String name;
+  private int price;
+  private String color;
+
+public TangiblrAsset(String name,int price,String color){
+  this.name=name;
+  this.price;
+  this.color;
+}
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+  public String getColor(){return this.corlor;}
+}
